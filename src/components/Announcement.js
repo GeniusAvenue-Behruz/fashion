85b28179e1268266184f71e5afb26d9128dae904
Behruz Rajabov <behruzgeniusavenue@gmail.com>
2024-10-28 09:47:21 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/Announcement.js 	new file:   src/components/Header.js 	modified:   src/index.css 	new file:   src/pages/Home.js 	new file:   src/pages/NoPage.js

---
+++ src/components/Announcement.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Announcement = () => {
+  return (
+      <div className='announcement'>Get early access on launches and offfer. <a href='#!'>Sign Up For Texts <i class="fa-solid fa-arrow-right"></i></a></div>
+  )
+}
+
+export default Announcement
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Announcements from './Announcement.js'
+const Header = () => {
+  return (
+      <div>
+      <Announcements/>
+      </div>
+  )
+}
+
+export default Header
